c4f51bc44ef30cbd13f0b09682f83d261b843463
Emilio Rojas <emilio93@gmail.com>
2017-03-23 22:57:49 -0600

    add tarea1

---
+++ tarea1/build.sh
@@ -0,0 +1,5 @@
+# Emilio Rojas 2017
+
+# Tarea 1.
+pdflatex --output-directory=$OUTPUTDIR/tarea1 -interaction=nonstopmode tarea1.tex
+pdflatex --output-directory=$OUTPUTDIR/tarea1 -interaction=nonstopmode tarea1.tex
