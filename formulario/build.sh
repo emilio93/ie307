8bdd3c4d21454aadab4aebc66e6d3abb728f1f7d
Emilio Rojas <emilio93@gmail.com>
2017-03-24 00:23:57 -0600

    updating formulario

--- formulario/build.sh
+++ formulario/build.sh
@@ -1,5 +1,6 @@
 # Emilio Rojas 2017
 
+mkdir -p $OUTPUTDIR/formulario1
 # Formulario primer parcial.
-pdflatex --output-directory=$OUTPUTDIR -interaction=nonstopmode parcial-i-formulario.tex
-pdflatex --output-directory=$OUTPUTDIR -interaction=nonstopmode parcial-i-formulario.tex
+pdflatex --output-directory=$OUTPUTDIR/formulario1 -interaction=nonstopmode formulario1.tex
+pdflatex --output-directory=$OUTPUTDIR/formulario1 -interaction=nonstopmode formulario1.tex
